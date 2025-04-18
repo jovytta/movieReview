56095d260a2b1bd584e0657f44c3f906feba5083
jovytta <jovyttayauwanta@gmail.com>
2025-04-18 09:56:49 +0800

    Initial Commit

---
+++ movieReview/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  movieReview
+//
+//  Created by Mac12 on 2025/4/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ movieReview/movieReviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  movieReviewApp.swift
+//  movieReview
+//
+//  Created by Mac12 on 2025/4/18.
+//
+
+import SwiftUI
+
+@main
+struct movieReviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
